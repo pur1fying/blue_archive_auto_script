9b07b423210f3f73d732f4834234ce25cf3bbc08
pur1fy <2274916027@qq.com>
2023-10-14 21:49:23 +0800

    small fix

--- module/common_solve_affection_story_method.py
+++ module/common_solve_affection_story_method.py
@@ -7,6 +7,8 @@ from module import common_skip_plot_method
 
 def implement(self):
     fail_cnt = 0
+    self.operation("swipe", [(924, 330), (924, 230)],duration = 0.1)
+    self.operation("click", (924, 330))
     while fail_cnt <= 4:
         path1 = "src/momo_talk/reply_button.png"
         path2 = "src/momo_talk/affection story.png"
@@ -17,7 +19,7 @@ def implement(self):
         print(return_data2)
         if return_data2[1][0] <= 1e-03:
             log.d("enter affection story", 1, logger_box=self.loggerBox)
-            self.operation("click", (return_data2[0][0] + 166, return_data2[0][1] + 45), duration=0.5)
+            self.operation("click", (return_data2[0][0] + 166, return_data2[0][1] + 45), duration=0.7)
             self.operation("click", (925, 564), duration=6)
             common_skip_plot_method.implement(self)
             return True
