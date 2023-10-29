854fa7e3b2c20dff532c29807109882924cfa0ac
Kiramei <1710623513@qq.com>
2023-10-29 16:11:58 +0800

    Special Daliy Config Fixed

--- gui/components/expand/specialDaily.py
+++ gui/components/expand/specialDaily.py
@@ -1,7 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIntValidator
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton, QVBoxLayout
-from qfluentwidgets import LineEdit
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QVBoxLayout
+from qfluentwidgets import ComboBox
 
 from gui.util.config_set import ConfigSet
 
@@ -14,37 +13,91 @@ class Layout(QWidget, ConfigSet):
         self.lay1 = QHBoxLayout(self)
         self.lay2 = QHBoxLayout(self)
 
-        self.label = QLabel('三个悬赏委托分别打的难度 0 1 2 3 ... 分别表示 A B C D 难度，与主线输入类似，1-1代表B难度打一次，列表中三个元素：', self)
-        self.input = LineEdit(self)
-        self.accept = QPushButton('确定', self)
+        self.option_1 = QHBoxLayout(self)
+        self.option_2 = QHBoxLayout(self)
+        self.option_3 = QHBoxLayout(self)
+
+        self.label_1_0 = QLabel('高架公路', self)
+        self.label_2_0 = QLabel('沙漠铁路', self)
+        self.label_3_0 = QLabel('讲堂', self)
+
+        self.input_1_1 = ComboBox(self)
+        self.input_2_1 = ComboBox(self)
+        self.input_3_1 = ComboBox(self)
+
+        self.input_1_2 = ComboBox(self)
+        self.input_2_2 = ComboBox(self)
+        self.input_3_2 = ComboBox(self)
+
+        self.input_1_1.addItems(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+        self.input_2_1.addItems(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+        self.input_3_1.addItems(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+
+        self.input_1_2.addItems(['1', '2'])
+        self.input_2_2.addItems(['1', '2'])
+        self.input_3_2.addItems(['1', '2'])
+
+        self.option_1.addWidget(self.label_1_0, 0, Qt.AlignLeft)
+        self.option_1.addStretch(1)
+        self.option_1.addWidget(self.input_1_1, 0, Qt.AlignRight)
+        self.option_1.addWidget(self.input_1_2, 0, Qt.AlignRight)
+
+        self.option_2.addWidget(self.label_2_0, 0, Qt.AlignLeft)
+        self.option_2.addStretch(1)
+        self.option_2.addWidget(self.input_2_1, 0, Qt.AlignRight)
+        self.option_2.addWidget(self.input_2_2, 0, Qt.AlignRight)
+
+        self.option_3.addWidget(self.label_3_0, 0, Qt.AlignLeft)
+        self.option_3.addStretch(1)
+        self.option_3.addWidget(self.input_3_1, 0, Qt.AlignRight)
+        self.option_3.addWidget(self.input_3_2, 0, Qt.AlignRight)
+
+        self.label = QLabel('请在下拉框中选择相应悬赏委托的难度和次数：', self)
 
         _set_main = self.get('specialPriority')
+        self.dif = [[int(y) for y in x.split('-')][0] for x in _set_main.split(',')]
+        self.count = [[int(y) for y in x.split('-')][1] for x in _set_main.split(',')]
 
-        self.setFixedHeight(120)
+        self.input_1_1.setCurrentIndex(self.dif[0])
+        self.input_1_2.setCurrentIndex(self.count[0] - 1)
+        self.input_2_1.setCurrentIndex(self.dif[1])
+        self.input_2_2.setCurrentIndex(self.count[1] - 1)
+        self.input_3_1.setCurrentIndex(self.dif[2])
+        self.input_3_2.setCurrentIndex(self.count[2] - 1)
 
-        self.input.setText(_set_main)
+        self.input_1_1.currentIndexChanged.connect(self._commit)
+        self.input_1_2.currentIndexChanged.connect(self._commit)
+        self.input_2_1.currentIndexChanged.connect(self._commit)
+        self.input_2_2.currentIndexChanged.connect(self._commit)
+        self.input_3_1.currentIndexChanged.connect(self._commit)
+        self.input_3_2.currentIndexChanged.connect(self._commit)
 
-        self.input.setFixedWidth(700)
+        self.setFixedHeight(200)
 
         self.lay1.setContentsMargins(10, 0, 0, 10)
-        self.lay2.setContentsMargins(10, 0, 0, 10)
-
-        self.accept.clicked.connect(self.__accept_main)
+        self.option_1.setContentsMargins(10, 0, 0, 10)
+        self.option_2.setContentsMargins(10, 0, 0, 10)
+        self.option_3.setContentsMargins(10, 0, 0, 10)
 
         self.lay1.addWidget(self.label, 0, Qt.AlignLeft)
-        self.lay2.addWidget(self.input, 1, Qt.AlignLeft)
-        self.lay2.addWidget(self.accept, 0, Qt.AlignLeft)
 
         self.lay1.addStretch(1)
         self.lay1.setAlignment(Qt.AlignCenter)
-        self.lay2.setAlignment(Qt.AlignCenter)
 
         self.hBoxLayout.addSpacing(16)
         self.hBoxLayout.setAlignment(Qt.AlignCenter)
 
         self.hBoxLayout.addLayout(self.lay1)
-        self.hBoxLayout.addLayout(self.lay2)
+        self.hBoxLayout.addLayout(self.option_1)
+        self.hBoxLayout.addLayout(self.option_2)
+        self.hBoxLayout.addLayout(self.option_3)
 
-    def __accept_main(self):
-        input_content = self.input.text()
-        self.set('specialPriority', input_content)
+    def _commit(self):
+        self.dif[0] = self.input_1_1.currentIndex()
+        self.dif[1] = self.input_2_1.currentIndex()
+        self.dif[2] = self.input_3_1.currentIndex()
+        self.count[0] = self.input_1_2.currentIndex() + 1
+        self.count[1] = self.input_2_2.currentIndex() + 1
+        self.count[2] = self.input_3_2.currentIndex() + 1
+        _formatted = ','.join([str(self.dif[i]) + '-' + str(self.count[i]) for i in range(0, 3)])
+        self.set('specialPriority', _formatted)
