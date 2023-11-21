e585877fcc01371b431c420f469f8716b043bde7
Scx <15518870899@163.com>
2023-11-21 18:53:18 +0800

    Add config check

--- gui/util/config_set.py
+++ gui/util/config_set.py
@@ -1,5 +1,7 @@
 import json
 
+from core.notification import notify
+
 
 class ConfigSet:
     def __init__(self):
@@ -13,3 +15,12 @@ class ConfigSet:
         self.config[key] = value
         with open('./config/config.json', 'w', encoding='utf-8') as f:
             json.dump(self.config, f, indent=4, ensure_ascii=False)
+        if not self.check(key, value):
+            notify('', '修改配置失败,请重新设置')
+            print(f'failed to set config {key}')
+
+    @staticmethod
+    def check(key, value):
+        with open('./config/config.json', 'r', encoding='utf-8') as f:
+            new_config = json.load(f)
+        return new_config.get(key) == value
