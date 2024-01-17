097d4e38bef4c3a2c2a56d85d4b8406e60459c4c
pur1fy <2274916027@qq.com>
2024-01-17 11:11:54 +0800

    fix

--- module/clear_special_task_power.py
+++ module/clear_special_task_power.py
@@ -1,6 +1,6 @@
 import time
 
-from core import color, image
+from core import color, image, picture
 from gui.util import log
 
 x = {
@@ -32,11 +32,8 @@ def implement(self):
             self.quick_method_to_main_page()
         if count[i] == "max" or count[i] > 0:
             self.logger.info("Start commissions task: " + commissions_name[i] + " count : " + str(count[i]))
-<<<<<<< Updated upstream
-=======
             if just_do_task:
                 to_request_select(self, True)
->>>>>>> Stashed changes
             just_do_task = True
             to_commissions(self, i + 1, skip_first_screenshot=True)
             res = commissions_common_operation(self, i + 1, count[i])
@@ -53,47 +50,7 @@ def implement(self):
 
 
 def start_sweep(self, skip_first_screenshot=False):
-<<<<<<< Updated upstream
-    if self.server == 'CN':
-        possibles = {
-            "special_task_task-info": (941, 411),
-        }
-        ends = [
-            "buy_ap_notice",
-            "normal_task_start-sweep-notice",
-        ]
-        res = image.detect(self, end=ends, possibles=possibles, skip_first_screenshot=skip_first_screenshot)
-        if res == "buy_ap_notice":
-            return "purchase_ap_notice"
-        possibles = {
-            "normal_task_start-sweep-notice": (765, 501)
-        }
-        ends = [
-            "normal_task_skip-sweep-complete",
-            "normal_task_sweep-complete",
-        ]
-        res = image.detect(self, end=ends, possibles=possibles, skip_first_screenshot=True,
-                           pre_func=color.detect_rgb_one_time, pre_argv=(self, [[640, 200]], ['level_up'], []))
-        if res == "normal_task_sweep-complete":
-            return "sweep_complete"
-        elif res == "normal_task_skip-sweep-complete":
-            return "skip_sweep_complete"
 
-    elif self.server == 'Global':
-        ends = [
-            "purchase_ap_notice",
-            "start_sweep_notice",
-        ]
-        res = color.common_rgb_detect_method(self, [[941, 411]], ["mission_info"],
-                                       ends, skip_first_screenshot=skip_first_screenshot)
-        if res == "purchase_ap_notice":
-            return res
-        ends = [
-            "skip_sweep_complete",
-            "sweep_complete",
-        ]
-        return color.common_rgb_detect_method(self, [[765, 501]], ["start_sweep_notice"], ends, True)
-=======
     img_ends = [
         "purchase_ap_notice",
         "normal_task_start-sweep-notice",
@@ -109,155 +66,13 @@ def start_sweep(self, skip_first_screenshot=False):
     img_possibles = {"normal_task_start-sweep-notice": (765, 501)}
     picture.co_detect(self, None,None, img_ends, img_possibles, skip_first_screenshot)
     return "sweep_complete"
->>>>>>> Stashed changes
-
-
-def to_commissions(self, num, skip_first_screenshot=False):
-    if self.server == 'CN':
-        possibles = {
-            "main_page_home-feature": (1198, 580),
-            "main_page_bus": (724, 581),
-            "special_task_request-select": (992, 0),
-            "special_task_task-info": (1085, 141),
-        }
-        if num == 1:
-            possibles["special_task_request-select"] = (992, 277)
-        elif num == 2:
-            possibles["special_task_request-select"] = (992, 406)
-        image.detect(self, 'special_task_level-list', possibles, skip_first_screenshot=skip_first_screenshot)
-
-    elif self.server == 'Global':
-        click_pos = [
-            [1198, 580],
-            [746, 515],
-            [992, 206],
-            [640, 116],
-            [1129, 142],
-            [886, 164],
-            [649, 508],
-
-        ]
-        if num == 1:
-            click_pos[2][1] = 206
-        elif num == 2:
-            click_pos[2][1] = 309
-        los = [
-            "main_page",
-            "campaign",
-            "choose_commissions",
-            "reward_acquired",
-            "mission_info",
-            "start_sweep_notice",
-            "skip_sweep_complete",
-        ]
-        ends = [
-            "commissions",
-        ]
-        color.common_rgb_detect_method(self, click_pos, los, ends, skip_first_screenshot=skip_first_screenshot)
 
 
 def commissions_common_operation(self, a, b):
-    click_pos = [
-        [1118, 0]
-    ]
-    pd_los = [
-        "commissions"
-    ]
-    ends = [
-        "mission_info",
-    ]
-    possibles = {
-        "special_task_level-list": (1118, 0)
-    }
-    i = 675
-    line = self.latest_img_array[:, 1076, :]
-    los = []
-    while i > 196:
-        if 131 <= line[i][2] <= 151 and 218 <= line[i][1] <= 238 and 245 <= line[i][0] <= 255 and \
-            131 <= line[i - 30][2] <= 151 and 218 <= line[i - 30][1] <= 238 and 245 <= line[i - 30][0] <= 255:
-            los.append(i - 35)
-            i -= 100
-        else:
-            i -= 1
-    for i in range(0, len(los)):
-        if self.server == 'CN':
-            possibles["special_task_level-list"] = (1118, los[i])
-            image.detect(self, 'special_task_task-info', possibles)
-        elif self.server == 'Global':
-            click_pos[0][1] = los[i]
-            color.common_rgb_detect_method(self, click_pos, pd_los, ends)
-        t = color.check_sweep_availability(self.latest_img_array, server=self.server)
-        if t == "sss":
-            if b == "max":
-                self.click(1085, 300, duration=1, wait_over=True)
-            else:
-                self.click(1014, 300, count=b - 1, duration=1, wait_over=True)
-            return start_sweep(self, skip_first_screenshot=True)
-        elif t == "no-pass" or t == "pass":
-            to_commissions(self, a, skip_first_screenshot=True)
-
-    self.swipe(926, 140, 926, 640, duration=1)
-    time.sleep(1)
-    self.latest_img_array = self.get_screenshot_array()
-    i = 675
-    line = self.latest_img_array[:, 1076, :]
-    los = []
-    while i > 196:
-        if 131 <= line[i][2] <= 151 and 218 <= line[i][1] <= 238 and 245 <= line[i][0] <= 255 and \
-            131 <= line[i - 30][2] <= 151 and 218 <= line[i - 30][1] <= 238 and 245 <= line[i - 30][0] <= 255:
-            los.append(i - 35)
-            i -= 100
-        else:
-            i -= 1
-    for i in range(0, len(los)):
-        if self.server == 'CN':
-            possibles["special_task_level-list"] = (1118, los[i])
-            image.detect(self, 'special_task_task-info', possibles, skip_first_screenshot=True)
-        elif self.server == 'Global':
-            click_pos[0][1] = los[i]
-            color.common_rgb_detect_method(self, click_pos, pd_los, ends, skip_first_screenshot=True)
-        t = color.check_sweep_availability(self.latest_img_array, server=self.server)
-        if t == "sss":
-            if b == "max":
-                self.click(1085, 300, duration=1, wait_over=True)
-            else:
-                self.click(1014, 300, count=b - 1, duration=1, wait_over=True)
-            return start_sweep(self, skip_first_screenshot=True)
-        elif t == "no-pass" or t == "pass":
-            to_commissions(self, a, skip_first_screenshot=True)
-
+    res = one_detect(self, a, b)
     self.swipe(926, 188, 926, 381, duration=1)
     time.sleep(1)
     self.latest_img_array = self.get_screenshot_array()
-<<<<<<< Updated upstream
-    line = self.latest_img_array[:, 1076, :]
-    i = 675
-    los = []
-    while i > 196:
-        if 131 <= line[i][2] <= 151 and 218 <= line[i][1] <= 238 and 245 <= line[i][0] <= 255 and \
-            131 <= line[i - 30][2] <= 151 and 218 <= line[i - 30][1] <= 238 and 245 <= line[i - 30][0] <= 255:
-            los.append(i - 35)
-            i -= 100
-        else:
-            i -= 1
-    for i in range(0, len(los)):
-        if self.server == 'CN':
-            possibles["special_task_level-list"] = (1118, los[i])
-            image.detect(self, 'special_task_task-info', possibles, skip_first_screenshot=True)
-        elif self.server == 'Global':
-            click_pos[0][1] = los[i]
-            color.common_rgb_detect_method(self, click_pos, pd_los, ends, skip_first_screenshot=True)
-        t = color.check_sweep_availability(self.latest_img_array, server=self.server)
-        if t == "sss":
-            if b == "max":
-                self.click(1085, 300, duration=1, wait_over=True)
-            else:
-                self.click(1014, 300, count=b - 1, duration=1, wait_over=True)
-            return start_sweep(self, skip_first_screenshot=True)
-        elif t == "no-pass" or t == "pass":
-            to_commissions(self, a, skip_first_screenshot=True)
-    return True
-=======
     return one_detect(self,a,b)
 
 
@@ -279,4 +94,3 @@ def to_request_select(self, skip_first_screenshot=False):
     rgb_possibles = {"main_page": (1198, 580)}
     picture.co_detect(self, None, rgb_possibles, img_ends, img_possibles,
                       skip_first_screenshot=skip_first_screenshot)
->>>>>>> Stashed changes
--- module/scrimmage.py
+++ module/scrimmage.py
@@ -1,155 +1,9 @@
 import time
 
-from core import color
+from core import color, picture
 
 
 def implement(self):
-    self.quick_method_to_main_page()
-    if self.server == 'CN':
-        self.logger.info('CN is not supported')
-    elif self.server == 'Global':
-        global_implement(self)
-    return True
-
-
-def start_sweep(self):
-    ends = [
-        "purchase_scrimmage_ticket",
-        "purchase_ap_notice",
-        "start_sweep_notice",
-    ]
-    res = color.common_rgb_detect_method(self, [[941, 411]], ["mission_info"], ends, True)
-    if res == "purchase_scrimmage_ticket" or res == "purchase_ap_notice":
-        return res
-    ends = [
-        "skip_sweep_complete",
-        "sweep_complete",
-        "purchase_scrimmage_ticket",
-        "purchase_ap_notice",
-    ]
-    return color.common_rgb_detect_method(self, [[765, 501]], ["start_sweep_notice"], ends, True)
-
-
-def scrimmage_common_operation(self, a, b):
-    self.latest_img_array = self.get_screenshot_array()
-    line = self.latest_img_array[:, 1076, :]
-    click_pos = [
-        [1118, 0]
-    ]
-    pd_los = [
-        "scrimmage"
-    ]
-    ends = [
-        "mission_info",
-    ]
-
-    los = []
-    i = 675
-    while i > 196:
-        if 131 <= line[i][2] <= 151 and 218 <= line[i][1] <= 238 and 245 <= line[i][0] <= 255 and \
-                131 <= line[i - 30][2] <= 151 and 218 <= line[i - 30][1] <= 238 and 245 <= line[i - 30][0] <= 255:
-            los.append(i - 35)
-            i -= 100
-        else:
-            i -= 1
-    print(los)
-    for i in range(0, len(los)):
-        click_pos[0][1] = los[i]
-        color.common_rgb_detect_method(self, click_pos, pd_los, ends, True)
-        t = color.check_sweep_availability(self.latest_img_array, server=self.server)
-        if t == "sss":
-            if b == "max":
-                self.click(1085, 300, wait=False, wait_over=True)
-                time.sleep(1)
-            else:
-                for j in range(0, b - 1):
-                    self.click(1014, 300, wait=False, wait_over=True)
-                    time.sleep(1)
-            res = start_sweep(self)
-            if res == "sweep_complete" or res == "skip_sweep_complete":
-                return "sweep_complete"
-            if res == "purchase_ap_notice":
-                return "inadequate_ap"
-            if res == "purchase_scrimmage_ticket":
-                return "inadequate_ticket"
-        elif t == "no-pass" or t == "pass":
-            to_scrimmage(self, a, True)
-
-    return True
-
-
-def to_scrimmage(self, num, skip_first_screenshot=False):
-    click_pos = [
-        [1198, 580],
-        [716, 599],
-        [992, 206],
-        [640, 116],
-        [920, 167],
-        [1129, 142],
-        [919, 164],
-        [649, 508],
-    ]
-    if num == 1:
-        click_pos[2][1] = 206
-    elif num == 2:
-        click_pos[2][1] = 309
-    elif num == 3:
-        click_pos[2][1] = 418
-    los = [
-        "main_page",
-        "campaign",
-        "choose_scrimmage",
-        "reward_acquired",
-        "purchase_scrimmage_ticket",
-        "mission_info",
-        "purchase_ap_notice",
-        "skip_sweep_complete"
-    ]
-    ends = [
-        "scrimmage",
-    ]
-    color.common_rgb_detect_method(self, click_pos, los, ends, skip_first_screenshot)
-
-
-def to_choose_scrimmage(self):
-    click_pos = [
-        [1198, 580],
-        [716, 599],
-    ]
-    los = [
-        "main_page",
-        "campaign",
-    ]
-    ends = [
-        "choose_scrimmage",
-    ]
-    color.common_rgb_detect_method(self, click_pos, los, ends, True)
-
-
-def purchase_scrimmage_ticket(self, times):
-    self.click(148, 101, wait=False,duration=1.5, wait_over=True)
-    if times == 12:  # max
-        self.click(879, 346, wait=False, wait_over=True)
-    else:
-        for i in range(0, times - 1):
-            self.click(807, 346, wait=False, wait_over=True)
-    click_pos = [
-        [766, 507],
-        [766, 507],
-        [640, 116],
-    ]
-    pd_los = [
-        "purchase_scrimmage_ticket",
-        "purchase_ticket_notice",
-        "reward_acquired",
-    ]
-    ends = [
-        "choose_scrimmage",
-    ]
-    color.common_rgb_detect_method(self, click_pos, pd_los, ends)
-
-
-def global_implement(self):
     scrimmage_area_name = ["Trinity", "Gehenna", "Millennium"]
     buy_ticket_times = min(self.config['purchase_scrimmage_ticket_times'], 12)  # ** 购买悬赏委托券的次数
 
@@ -167,6 +21,8 @@ def global_implement(self):
             self.quick_method_to_main_page()
         if (count[i] == "max" or count[i] > 0) and not self.scrimmage_task_status[i]:
             self.logger.info("Start scrimmage task: " + scrimmage_area_name[i] + " count : " + str(count[i]))
+            if just_do_task:
+                to_choose_scrimmage(self,True)
             just_do_task = True
             to_scrimmage(self, i + 1, True)
             res = scrimmage_common_operation(self, i + 1, count[i])
@@ -180,8 +36,6 @@ def global_implement(self):
                 return True
     self.logger.info(str(self.scrimmage_task_status))
     return True
-<<<<<<< Updated upstream
-=======
 
 
 def start_sweep(self):
@@ -261,7 +115,7 @@ def to_scrimmage(self, num, skip_first_screenshot=False):
     picture.co_detect(self, rgb_ends, rgb_possibles, img_ends, img_possibles, skip_first_screenshot)
 
 
-def to_choose_scrimmage(self):
+def to_choose_scrimmage(self, skip_first_screenshot=False):
     rgb_ends = "choose_scrimmage"
     rgb_possibles = {"main_page": (1198, 580)}
     img_ends = "scrimmage_academy-select"
@@ -269,7 +123,7 @@ def to_choose_scrimmage(self):
         "main_page-home-feature": (1198, 580),
         "main_page-bus": (716, 599),
     }
-    picture.co_detect(self, rgb_ends, rgb_possibles, img_ends, img_possibles)
+    picture.co_detect(self, rgb_ends, rgb_possibles, img_ends, img_possibles, skip_first_screenshot)
 
 
 def purchase_scrimmage_ticket(self, times):
@@ -290,4 +144,3 @@ def purchase_scrimmage_ticket(self, times):
         "rewarded_task_purchase-scrimmage-ticket-notice": (766, 507),
     }
     picture.co_detect(self, rgb_ends, rgb_possibles, img_ends, img_possibles, skip_first_screenshot=False)
->>>>>>> Stashed changes
