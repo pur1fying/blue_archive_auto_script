6e89812696bb5ba8e06ab1c225226f5ba30221e8
pur1fy <2274916027@qq.com>
2024-03-08 00:37:27 +0800

    add necessary pictures for total assault in Global server

--- src/images/Global/x_y_range/total_assault.py
+++ src/images/Global/x_y_range/total_assault.py
@@ -10,5 +10,8 @@ x_y_range = {
     'battle-lost-confirm': (577, 636, 700, 683),
     'battle-win-confirm': (1120, 646, 1222, 681),
     'inadequate-ticket': (438, 311, 824, 351),
-    "give-up-notice": (602, 307, 683, 379)
+    "give-up-notice": (602, 307, 683, 379),
+    'win-reward-confirm': (713, 637, 832, 680),
+    'total-assault-result': (400, 109, 828, 145),
+    'reach-season-highest-record': (400, 146, 902, 183)
 }
