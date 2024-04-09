e8f2d50e6e8e01d3c0a1211d63d13b8ba4297924
pur1fy <2274916027@qq.com>
2024-04-09 12:50:57 +0800

    delete Union expression to run the code in python 3.9

--- gui/util/translator.py
+++ gui/util/translator.py
@@ -15,13 +15,13 @@ class LanguageSerializer(ConfigSerializer):
 
     def deserialize(self, value: str):
         return Language(QLocale(value))
-    
+
 
 class Config(QConfig):
     """ Language config """
     language = OptionsConfigItem(
         "Translator", "Language", Language.ENGLISH, OptionsValidator(Language), LanguageSerializer(), restart=True)
-    
+
     def __init__(self):
         super().__init__()
 
@@ -35,37 +35,37 @@ class Translator(QTranslator):
         self.locale = self.cfg.get(self.cfg.language).value
         self.stringLang = self.locale.name()
         self.__config_translation = None
-        
+
     def loadCfgTranslation(self):
         self.__config_translation = ConfigTranslation()
 
     def isString(self, value):
         return isinstance(value, str)
-    
+
     def isBytes(self, value):
         return isinstance(value, bytes)
-    
-    def toString(self, tranlation: str | bytes) -> str:
+
+    def toString(self, tranlation) -> str:
         if self.isBytes(tranlation):
             tranlation = self.decode(tranlation)
         return tranlation
-    
+
     def encode(self, *args):
         return [arg.encode('utf-8') if self.isString(arg) else arg for arg in args]
 
     def decode(self, *args):
         return [arg.decode('utf-8') if self.isBytes(arg) else arg for arg in args]
-    
+
     def __get(self, text):
         return self.__config_translation.entries.get(text)
-    
+
     def isChinese(self):
         return self.stringLang == 'zh_CN'
 
-    def tr(self, 
-           context: str, 
-           sourceText: str, 
-           disambiguation: str | None = None, 
+    def tr(self,
+           context: str,
+           sourceText: str,
+           disambiguation: str = '',
            n: int = -1) -> str:
         """
         Translate sourceText by looking in the qm file.
@@ -73,10 +73,10 @@ class Translator(QTranslator):
 
         Parameters
         ----------
-        context: str 
+        context: str
             context tag in .ts file e.g ConfigTranslation
 
-        sourceText: str 
+        sourceText: str
             text to translate
         """
         if not self.isChinese() and self.isString(sourceText) and self.isString(context):
@@ -85,14 +85,14 @@ class Translator(QTranslator):
             if translation:
                 return self.toString(translation)
         return sourceText
-    
+
     def undo(self, text: str) -> str:
         """
         Undo translations by looking in ConfigTranslation.
 
         Parameters
         ----------
-        text: str 
+        text: str
             text to undo translation
         """
         if not self.isChinese() and self.isString(text) and self.__get(text):
