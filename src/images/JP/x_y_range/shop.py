42d2ffe281cf719be782981d8ba16d2b6deb129e
pur1fy <2274916027@qq.com>
2024-05-23 19:28:50 +0800

    fix : [ JP ] ui change : shop_refresh-notice and group_enter-button

--- src/images/JP/x_y_range/shop.py
+++ src/images/JP/x_y_range/shop.py
@@ -2,7 +2,7 @@ prefix = "shop"
 path = "shop"
 x_y_range = {
     'confirm': (730, 468, 803, 509),
-    'refresh-notice': (451, 267, 827, 303),
+    'refresh-notice': (431, 237, 857, 333),
     'refresh-unavailable-notice': (427, 312, 841, 351),
     'menu': (100, 7, 262, 40),
     'purchase-notice1': (434, 423, 844, 459),
