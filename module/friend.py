53c62b400f80fb456f43e96c16bc5dc9ee266fb6
pur1fy <2274916027@qq.com>
2024-08-17 23:12:21 +0800

    update friend management

--- module/friend.py
+++ module/friend.py
@@ -4,6 +4,9 @@ from core import picture, color
 
 
 def implement(self):
+    if self.server == 'Global' or self.server == 'JP':
+        self.logger.info("Friend Management Not Support In Global And JP Server")
+        return True
     self.quick_method_to_main_page()
     to_friend_management(self, True)
     self.logger.info("Clear Friend White List : + " + str(self.config.get("clear_friend_white_list")))
@@ -49,6 +52,7 @@ def implement(self):
         to_friend_management(self)
     return True
 
+
 def to_friend_management(self, skip_first_screenshot=False):
     img_ends = "friend_friend-management-menu"
     img_possibles = {
@@ -56,18 +60,59 @@ def to_friend_management(self, skip_first_screenshot=False):
         "main_page_menu": (537, 467),
         "friend_player-info": (903, 101),
         "friend_delete-friend-notice": (887, 165),
+        "group_enter-button": (627, 383),
     }
-    rgb_possibles = {"main_page": (1226, 39)}
+    main_page_click_position = {
+        'CN': (1226, 39),
+        'Global': (562, 659),
+        'JP': (562, 659)
+    }
+    rgb_possibles = {"main_page": main_page_click_position[self.server]}
     picture.co_detect(self, None, rgb_possibles, img_ends, img_possibles, skip_first_screenshot=skip_first_screenshot)
 
 
 def get_possible_friend_positions(self):
+    funcs = {
+        'CN': search_cn,
+        'Global': search_global,
+        'JP': search_jp
+    }
+    return funcs[self.server](self)
+
+
+def search_cn(self):
     found_list = []
     i = 157
     x = 1183
     while i < 595:
         if color.judge_rgb_range(self, x, i, 34, 54, 60, 80, 88, 108):
             found_list.append((1183, i))
+            i += 50
+        else:
+            i += 1
+    return found_list
+
+
+def search_global(self):
+    found_list = []
+    i = 157
+    x = 487
+    while i < 595:
+        if color.judge_rgb_range(self, x, i, 250, 255, 250, 255, 250, 255) and color.judge_rgb_range(self, x, i + 18, 250, 255, 250, 255, 250, 255):
+            found_list.append((493, i + 9))
+            i += 50
+        else:
+            i += 1
+    return found_list
+
+
+def search_jp(self):
+    found_list = []
+    i = 157
+    x = 487
+    while i < 595:
+        if color.judge_rgb_range(self, x, i, 250, 255, 250, 255, 250, 255) and color.judge_rgb_range(self, x, i + 18, 250, 255, 250, 255, 250, 255):
+            found_list.append((493, i + 9))
             i += 50
         else:
             i += 1
@@ -85,7 +130,7 @@ def to_player_info(self, position):
 def check_name_in_white_list(self):
     ocr_region = {
         'CN': (680, 385, 747, 409),
-        'Global': (680, 385, 747, 409),
+        'Global': (711, 394, 823, 419),
         'JP': (680, 385, 747, 409),
     }
     white_list = self.config.get("clear_friend_white_list")
