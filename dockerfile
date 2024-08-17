335d636060cce6739f8008fefa9c92c773e526e6
Kiramei <1710623513@qq.com>
2024-08-18 00:45:09 +0800

    Fix: Update the docker settings

--- dockerfile
+++ dockerfile
@@ -1,5 +1,22 @@
-# Use the official Python base image
-FROM python:3.9-slim
+# Use the official Debian image as the base
+FROM hub.atomgit.com/amd64/debian:latest
+
+# Remove the original mirror
+RUN rm -rf /etc/apt/sources.list.d
+
+# Alter the source to tsinghua mirror
+RUN echo "deb http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware\ndeb http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware\ndeb http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware\ndeb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware" > /etc/apt/sources.list
+
+# Install Git Env
+RUN apt-get update && apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget && apt-get install -y git
+
+# Install Python Environment
+RUN wget https://mirrors.aliyun.com/python-release/source/Python-3.9.18.tar.xz
+RUN tar -xvJf Python-3.9.18.tar.xz
+RUN cd Python-3.9.18 && ./configure --enable-optimizations
+RUN cd Python-3.9.18 && make -j $(nproc) && make altinstall
+RUN rm -rf Python-3.9.18.tar.xz Python-3.9.18
+
 
 # Set the working directory in the container
 WORKDIR /app
@@ -7,11 +24,8 @@ WORKDIR /app
 # Copy the requirements file to the container
 COPY requirements.txt .
 
-# Install the Python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
-
 # Copy the rest of the application code to the container
 COPY . .
 
 # Set the command to run the application
-CMD ["entrypoint.sh"]
+CMD ["bash", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,86 @@
 #!/bin/bash
 
-# 设置虚拟环境的名称和临时文件夹的名称
-VENV_NAME="myenv"
-TEMP_FOLDER="temp_build"
+# Check the version
+GIT_HOME="/usr/bin/git"  # 修改为你的 git 可执行文件路径
+REPO_URL_HTTP="https://gitee.com/pur1fy/blue_archive_auto_script.git"  # 修改为你的仓库地址
 
-# 创建临时文件夹
-mkdir -p "$TEMP_FOLDER"
+# 设置虚拟环境的名称
+VENV_NAME="env"
 
-# 创建虚拟环境
-python3 -m venv "$TEMP_FOLDER/$VENV_NAME"
+# 检查虚拟环境是否已经存在
+if [ -d "$VENV_NAME" ]; then
+    echo "[INFO] Virtual environment '$VENV_NAME' already exists."
+else
+    # 创建虚拟环境
+    echo "[INFO] Creating virtual environment '$VENV_NAME'..."
+    python3.9 -m venv "$VENV_NAME"
+
+    # 检查虚拟环境是否成功创建
+    if [ ! -d "$VENV_NAME" ]; then
+        echo "[ERROR] Failed to create the virtual environment."
+        exit 1
+    fi
+fi
 
 # 激活虚拟环境
-source "$TEMP_FOLDER/$VENV_NAME/bin/activate"
+echo "[INFO] Activating virtual environment '$VENV_NAME'..."
+source "$VENV_NAME/bin/activate"
 
-# 安装所需的 Python 包
-pip install pyinstaller
-pip install requests
-pip install tqdm
+# 确认虚拟环境已激活
+if [ "$VIRTUAL_ENV" != "" ]; then
+    echo "[INFO] Virtual environment '$VENV_NAME' activated successfully."
+else
+    echo "[ERROR] Failed to activate the virtual environment."
+    exit 1
+fi
 
-# 使用 PyInstaller 打包应用程序
-pyinstaller -i gui/assets/logo.ico --name BlueArchiveAutoScript -F installer.py
+echo "+--------------------------------+"
+echo "|          UPDATE BAAS           |"
+echo "+--------------------------------+"
 
-# 删除临时文件夹
-rm -rf "$TEMP_FOLDER"
+remote_sha=$($GIT_HOME ls-remote --heads origin refs/heads/master | awk '{print $1}')
+local_sha=$($GIT_HOME rev-parse HEAD)
+
+echo "[INFO] Remote SHA: $remote_sha"
+echo "[INFO] Local SHA: $local_sha"
+
+if [ "$local_sha" = "$remote_sha" ] && [ -z "$($GIT_HOME diff)" ]; then
+    echo "[INFO] No updates available"
+else
+    echo "[INFO] Pulling updates from the remote repository..."
+    $GIT_HOME reset --hard HEAD
+    $GIT_HOME pull "$REPO_URL_HTTP"
+
+    updated_local_sha=$($GIT_HOME rev-parse HEAD)
+
+    echo "[INFO] Updated SHA: $updated_local_sha"
+    if [ "$updated_local_sha" = "$remote_sha" ]; then
+        echo "[INFO] Update success"
+    else
+        echo "[ERROR] Failed to update the source code, please check your network or for conflicting files"
+    fi
+fi
+
+ATX_APK_URL="https://gitee.com/pur1fy/blue_archive_auto_script_assets/raw/master/ATX.apk"
+
+APK_PATH="/app/src/atx_app/ATX.apk"
+
+echo "[INFO] Checking atx-agent..."
+
+# Check if ATX.apk exists
+if [ ! -f "$APK_PATH" ]; then
+    echo "[INFO] Downloading atx-agent..."
+    # Download ATX.apk
+    wget -O "$APK_PATH" "$ATX_APK_URL"
+else
+    echo "[INFO] Atx-agent already downloaded"
+fi
+
+
+# Install the env
+echo "[INFO] Check and Update the runtime environment..."
+pip3.9 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+# Start the app
+echo "[INFO] Starting the app..."
+python3.9 window.py
