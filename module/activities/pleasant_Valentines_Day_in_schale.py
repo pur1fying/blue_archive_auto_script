dfa2f171c8b46c586ac22837477ac5905ab41316
pur1fy <2274916027@qq.com>
2024-11-01 17:00:18 +0800

    Update pleasant_Valentines_Day_in_schale.py

--- module/activities/pleasant_Valentines_Day_in_schale.py
+++ module/activities/pleasant_Valentines_Day_in_schale.py
@@ -199,8 +199,8 @@ def explore_challenge(self):
     self.quick_method_to_main_page()
     to_activity(self, "challenge", True, True)
     tasks = [
-        # "challenge2_sss",
-        # "challenge2_task",
+        "challenge2_sss",
+        "challenge2_task",
         "challenge4_sss",
         "challenge4_task"
     ]
