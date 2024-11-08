ca8acf6b00806b3d7c9d34e7c9c4b0fa6cad59b1
pur1fy <2274916027@qq.com>
2024-11-08 20:04:33 +0800

    update : Global drill auto fight and sweep

--- src/images/CN/x_y_range/joint_firing_drill.py
+++ src/images/CN/x_y_range/joint_firing_drill.py
@@ -2,23 +2,26 @@ prefix = 'drill'
 path = 'joint_firing_drill'
 x_y_range = {
     "select-drill-menu": (97, 6, 312, 61),
+    "Assault-fighting": (92, 126, 172, 150),
     "Assault-open": (96, 162, 171, 181),
     "Assault-lock": (118, 235, 253, 282),
     "Assault-next": (),
-    "Assault-fighting": (92, 126, 172, 150),
+
+    "Defense-fighting": (648, 268, 781, 307),
     "Defense-open": (),
     "Defense-lock": (648, 268, 781, 307),
     "Defense-next": (),
-    "Defense-fighting": (648, 268, 781, 307),
+
+    "Shooting-fighting": (1021, 141, 1098, 161),
     "Shooting-open": (),
     "Shooting-lock": (),
     "Shooting-next": (1021, 141, 1098, 161),
-    "Shooting-fighting": (1021, 141, 1098, 161),
+
     "Season-Record": (30, 581, 179, 606),
     "drill-information": (574, 135, 710, 178),
     "start-drill-notice": (441, 322, 821, 393),
     "drill-finish": (577, 109, 705, 147),
     "sweep-menu": (571, 179, 705, 215),
-    "start-sweep-notice": (597, 141, 674, 179),
+    "start-sweep-notice": (587, 141, 700, 179),
     "sweep-complete": (601, 561, 682, 604)
 }
