a7f4e8ab6036a4b45408f1647e4fbca5c90c687a
pur1fy <2274916027@qq.com>
2025-01-23 00:53:39 +0800

    update: [CN] drill_Shooting-next

--- src/images/CN/x_y_range/joint_firing_drill.py
+++ src/images/CN/x_y_range/joint_firing_drill.py
@@ -16,8 +16,7 @@ x_y_range = {
     "Shooting-fighting": (1020, 108, 1100, 127),
     "Shooting-open": (1021, 141, 1098, 161),
     "Shooting-lock": (1046, 217, 1180, 261),
-
-    "Shooting-next": (1021, 141, 1098, 161),
+    "Shooting-next": (1017, 162, 1098, 186),
 
     "fight-1-finished": (747, 650, 777, 686),
     "fight-1-unfinished": (747, 650, 777, 686),
