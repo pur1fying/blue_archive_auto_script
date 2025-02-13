9ee7473b65d676ac7c3fe9c5f1520d9f3a194f1b
pur1fy <2274916027@qq.com>
2025-02-13 20:35:42 +0800

    update : [CN] activity

--- core/default_config.py
+++ core/default_config.py
@@ -3082,7 +3082,7 @@ STATIC_DEFAULT_CONFIG = '''
         ]
     },
     "current_game_activity": {
-        "CN": "SweetSecretsAndGunfightsATaleOfAfterSchoolSweets",
+        "CN": "anUnconcealedHeart",
         "Global": null,
         "JP": "CodeBox"
     },
