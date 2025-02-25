d1d114c968d13b42e5fb65d1c8a0aaadcaf40ff1
pur1fy <2274916027@qq.com>
2025-02-25 11:57:58 +0800

    bug fix

--- gui/components/expand/mainlinePriority.py
+++ gui/components/expand/mainlinePriority.py
@@ -91,7 +91,10 @@ class Layout(QWidget):
             input_content = input_content.split(',')
             temp = []
             for i in range(0, len(input_content)):
-                temp.append(readOneNormalTask(input_content[i],self.static_config.explore_normal_task_region_range))
+                temp.append(readOneNormalTask(
+                    input_content[i],
+                    self.config.static_config.explore_normal_task_region_range)
+                )
             self.config.set("unfinished_normal_tasks", temp)  # refresh the config unfinished_normal_tasks
             notification.success(self.tr('设置成功'), f'{self.tr("你的普通关卡已经被设置为：")}{input_content}',
                                  self.config)
@@ -106,7 +109,11 @@ class Layout(QWidget):
             input_content = input_content.split(',')
             temp = []
             for i in range(0, len(input_content)):
-                temp.append(readOneHardTask(input_content[i], self.static_config.explore_hard_task_region_range))
+                temp.append(readOneHardTask(
+                            input_content[i],
+                            self.config.static_config.explore_hard_task_region_range
+                    )
+                )
             self.config.set("unfinished_hard_tasks", temp)  # refresh the config unfinished_hard_tasks
             notification.success(self.tr('设置成功'), f'{self.tr("你的困难关卡已经被设置为：")}{input_content}',
                                  self.config)
