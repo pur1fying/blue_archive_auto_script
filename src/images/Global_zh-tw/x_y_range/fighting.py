033f9d41984b87f72b1f54d216fa72a0784afc3a
pur1fy <2274916027@qq.com>
2025-03-10 00:16:22 +0800

    zh-tw group_story templates

--- src/images/Global_zh-tw/x_y_range/fighting.py
+++ src/images/Global_zh-tw/x_y_range/fighting.py
@@ -1,7 +1,7 @@
 prefix = "fighting"
 path = "fighting/common"
 x_y_range = {
-    "pause-button": (1219,28,1252,64),
-    "pause-notice": (586,140,695,180),
-    "retreat-notice": (428,311,863,351),
+    "pause-button": (1219, 28, 1252, 64),
+    "pause-notice": (586, 140, 695, 180),
+    "retreat-notice": (428, 311, 863, 351),
 }
