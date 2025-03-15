a59384b9f790aa635e7300e206626c699fae6ac3
pur1fy <2274916027@qq.com>
2025-03-15 22:46:58 +0800

    zh-tw activity_common templates

--- src/images/Global_zh-tw/x_y_range/activity_common.py
+++ src/images/Global_zh-tw/x_y_range/activity_common.py
@@ -1,12 +1,12 @@
 prefix = 'activity'
 path = 'activity/common'
 x_y_range = {
-    'play-guide': (535, 132, 784, 171),
-    'menu': (103, 7, 250, 38),
+    'play-guide': (535, 132, 659, 171),
+    'menu': (103, 7, 160, 38),
     "fight-success-confirm": (580, 650, 701, 684),
-    "unit-formation": (102, 10, 317, 40),
-    'task-info': (548, 124, 732, 153),
-    'task-no-goals': (252, 387, 619, 411),
+    # "unit-formation": (102, 10, 317, 40),
+    'task-info': (578, 124, 702, 153),
+    # 'task-no-goals': (252, 387, 619, 411),
     'formation': (103, 7, 250, 38),
     'quest-challenge-button': (317, 255, 455, 288),
     'story-enter-task-button': (1094, 170, 1162, 215),
