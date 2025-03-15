e064d3db060b393fcc7c262b9970b1e286a618d1
pur1fy <2274916027@qq.com>
2025-03-16 00:03:15 +0800

    zh-tw scrimmage templates

--- src/images/Global_zh-tw/x_y_range/scrimmage.py
+++ src/images/Global_zh-tw/x_y_range/scrimmage.py
@@ -3,7 +3,7 @@ path = "scrimmage"
 x_y_range = {
     "academy-select": (649, 90, 892, 128),
     'level-list': (862, 98, 999, 123),
-    'task-info': (528, 124, 762, 153),
-    'purchase-scrimmage-ticket-menu': (424, 150, 850, 189),
-    'purchase-scrimmage-ticket-notice': (424, 159, 850, 201)
+    'task-info': (578, 124, 712, 153),
+    'purchase-scrimmage-ticket-menu': (494, 150, 780, 189),
+    'purchase-scrimmage-ticket-notice': (494, 159, 780, 201)
 }
