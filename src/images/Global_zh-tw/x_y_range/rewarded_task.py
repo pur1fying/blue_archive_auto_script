062a5f453e1211d24e6c3f33a208472278b12aed
pur1fy <2274916027@qq.com>
2025-03-16 00:03:39 +0800

    zh-tw rewarded_task templates

--- src/images/Global_zh-tw/x_y_range/rewarded_task.py
+++ src/images/Global_zh-tw/x_y_range/rewarded_task.py
@@ -3,7 +3,7 @@ path = "rewarded_task"
 x_y_range = {
     "location-select": (649, 90, 892, 128),
     'level-list': (862, 98, 999, 123),
-    'task-info': (528, 124, 762, 153),
-    'purchase-bounty-ticket-menu': (424, 150, 850, 189),
-    'purchase-bounty-ticket-notice': (424, 159, 850, 201)
+    'task-info': (578, 124, 712, 153),
+    'purchase-bounty-ticket-menu': (514, 150, 760, 189),
+    'purchase-bounty-ticket-notice': (514, 159, 760, 201)
 }
