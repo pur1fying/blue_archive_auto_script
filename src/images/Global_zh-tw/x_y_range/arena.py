a1c1ecfeab2b223ebeb11eed6fe099431b86e80e
pur1fy <2274916027@qq.com>
2025-03-16 12:49:43 +0800

    update zh-tw arena template

--- src/images/Global_zh-tw/x_y_range/arena.py
+++ src/images/Global_zh-tw/x_y_range/arena.py
@@ -7,6 +7,6 @@ x_y_range = {
     'battle-win': (580, 124, 700, 162),
     'battle-lost': (580, 191, 700, 228),
     'purchase-tactical-challenge-ticket': (411, 240, 877, 275),
-    'season-record': (400, 137, 900, 172),
+    'season-record': (500, 137, 773, 172),
     'best-record': (453, 139, 815, 172)
 }
