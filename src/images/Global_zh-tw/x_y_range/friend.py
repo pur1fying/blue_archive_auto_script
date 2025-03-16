af759408421c8268fef8e2a979278d9867761e1e
pur1fy <2274916027@qq.com>
2025-03-16 23:02:44 +0800

    update zh-tw friend template

--- src/images/Global_zh-tw/x_y_range/friend.py
+++ src/images/Global_zh-tw/x_y_range/friend.py
@@ -2,7 +2,7 @@ prefix = "friend"
 path = "friend"
 x_y_range = {
     'friend-menu': (100, 8, 220, 37),
-    'friend-management-menu': (100, 8, 261, 37),
-    'player-info': (547, 86, 723, 121),
-    'delete-friend-notice': (487, 315, 772, 347)
+    'player-info': (577, 86, 703, 121),
+    'delete-friend-notice': (487, 315, 782, 347),
+    'delete-friend': (1100, 225, 1186, 258)
 }
