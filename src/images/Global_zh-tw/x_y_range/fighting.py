b0894a3b7e3525bd7d73f42d6ec34143dc41b9e5
pur1fy <2274916027@qq.com>
2025-03-16 23:16:28 +0800

    update zh-tw fighting template

--- src/images/Global_zh-tw/x_y_range/fighting.py
+++ src/images/Global_zh-tw/x_y_range/fighting.py
@@ -2,6 +2,6 @@ prefix = "fighting"
 path = "fighting/common"
 x_y_range = {
     "pause-button": (1219, 28, 1252, 64),
-    "pause-notice": (586, 140, 695, 180),
-    "retreat-notice": (428, 311, 863, 351),
+    "pause-notice": (586, 120, 695, 180),
+    "retreat-notice": (458, 311, 803, 351),
 }
