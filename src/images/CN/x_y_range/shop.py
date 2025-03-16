944cc984295417de223ebfdedfe414fb5dc5e43f
pur1fy <2274916027@qq.com>
2025-03-16 22:41:05 +0800

    update zh-tw shop template

--- src/images/CN/x_y_range/shop.py
+++ src/images/CN/x_y_range/shop.py
@@ -1,7 +1,6 @@
 prefix = "shop"
 path = "shop"
 x_y_range = {
-    'confirm': (737, 446, 766, 476),
     'refresh-notice': (575, 270, 628, 302),
     'refresh-unavailable-notice': (547, 315, 600, 350),
     'menu': (100, 7, 262, 40),
--- src/images/Global_ko-kr/x_y_range/shop.py
+++ src/images/Global_ko-kr/x_y_range/shop.py
@@ -1,7 +1,6 @@
 prefix = "shop"
 path = "shop"
 x_y_range = {
-    'confirm': (730, 468, 803, 509),
     'refresh-notice': (483, 270, 804, 302),
     'refresh-unavailable-notice': (481, 310, 797, 355),
     'menu': (100, 7, 262, 40),
