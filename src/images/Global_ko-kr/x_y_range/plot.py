42d67913c1de1e3fd4b73dbf99d32a1b4f370182
pur1fy <2274916027@qq.com>
2025-03-17 21:48:55 +0800

    update ko-kr plot templates

--- src/images/Global_ko-kr/x_y_range/plot.py
+++ src/images/Global_ko-kr/x_y_range/plot.py
@@ -3,9 +3,9 @@ path = "plot"
 x_y_range = {
     'menu': (1162, 21, 1250, 56),
     'skip-plot-button': (1193, 103, 1235, 132),
-    'skip-plot-notice': (566, 124, 715, 160),
-    "formation": (100, 9, 340, 40),
-    "self-formation": (100, 9, 340, 40),
-    "change-unit-formation": (450, 74, 835, 108),
+    'skip-plot-notice': (611, 124, 668, 160),
+    "formation": (100, 9, 250, 40),
+    "self-formation": (100, 9, 250, 40),
+    "change-unit-formation": (578, 74, 701, 108),
 
 }
