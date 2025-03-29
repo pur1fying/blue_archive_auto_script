4e97eb95030a814a9da1e8bf425ebe081e777bc6
pur1fy <2274916027@qq.com>
2025-03-30 02:17:45 +0800

    enable delete friend in Global server

--- src/images/Global_ko-kr/x_y_range/friend.py
+++ src/images/Global_ko-kr/x_y_range/friend.py
@@ -1,7 +1,8 @@
 prefix = "friend"
 path = "friend"
 x_y_range = {
-    'friend-management-menu': (100, 8, 261, 37),
+    'friend-management-menu': (100, 8, 191, 37),
     'player-info': (547, 86, 723, 121),
-    'delete-friend-notice': (487, 315, 772, 347)
+    'delete-friend-notice': (457, 315, 812, 347),
+    'delete-friend': (1100, 225, 1186, 258)
 }
