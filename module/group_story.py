84b690eee422b1ade860c7968194501f20c75181
pur1fy <2274916027@qq.com>
2025-03-30 00:06:06 +0800

    update ko-kr gorup_story templates

--- module/group_story.py
+++ module/group_story.py
@@ -17,8 +17,7 @@ def implement(self):
                 while (not check_current_episode_cleared(self)) and self.flag_run:
                     res = one_detect(self)
                     while not res:
-                        time.sleep(self.screenshot_interval)
-                        self.latest_img_array = self.get_screenshot_array()
+                        self.update_screenshot_array()
                         res = one_detect(self)
                         continue
                     to_episode_info(self, res, True)
--- src/images/Global_ko-kr/x_y_range/group_story.py
+++ src/images/Global_ko-kr/x_y_range/group_story.py
@@ -1,10 +1,10 @@
 prefix = 'group_story'
 path = 'group_story'
 x_y_range = {
-    "select-episode": (100, 9, 350, 41),
-    "enter-group-story": (966, 367, 1123, 403),
-    "menu": (100, 9, 266, 41),
-    "episode-cleared-feature": (845, 182, 1018, 212),
-    "episode-info": (500, 145, 740, 174)
+    "menu": (100, 9, 240, 41),
+    "select-episode": (100, 9, 180, 41),
+    "enter-group-story": (996, 367, 1123, 403),
+    "episode-cleared-feature": (805, 182, 1048, 212),
+    "episode-info": (550, 145, 720, 174)
 
 }
