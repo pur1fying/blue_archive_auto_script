6c970e4a7eaf85ba41e0fb2e44e3175bd67ccb08
pur1fy <2274916027@qq.com>
2025-03-30 15:54:31 +0800

    update ko-kr special_task templates

--- src/images/Global_ko-kr/x_y_range/special_task.py
+++ src/images/Global_ko-kr/x_y_range/special_task.py
@@ -3,5 +3,5 @@ path = "special_task"
 x_y_range = {
     "request-select": (649, 90, 892, 128),
     'level-list': (862, 98, 999, 123),
-    'task-info': (528, 124, 762, 153),
+    'task-info': (588, 124, 702, 153),
 }
