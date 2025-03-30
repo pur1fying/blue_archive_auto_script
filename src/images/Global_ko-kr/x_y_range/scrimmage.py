3c58e1fd70fbe2708f7fee4872d24bce60dcdcb9
pur1fy <2274916027@qq.com>
2025-03-30 15:54:44 +0800

    update ko-kr scrimmage templates

--- src/images/Global_ko-kr/x_y_range/scrimmage.py
+++ src/images/Global_ko-kr/x_y_range/scrimmage.py
@@ -3,7 +3,7 @@ path = "scrimmage"
 x_y_range = {
     "academy-select": (649, 90, 892, 128),
     'level-list': (862, 98, 999, 123),
-    'task-info': (528, 124, 762, 153),
-    'purchase-scrimmage-ticket-menu': (424, 150, 850, 189),
-    'purchase-scrimmage-ticket-notice': (424, 159, 850, 201)
+    'task-info': (588, 124, 702, 153),
+    'purchase-scrimmage-ticket-menu': (514, 150, 770, 189),
+    'purchase-scrimmage-ticket-notice': (514, 159, 770, 201)
 }
