ffc59c491eb6cb0f5587dbf64dff10243c1e9309
pur1fy <2274916027@qq.com>
2025-04-04 13:31:17 +0800

    fix : zh-tw cafe-reward-status template

--- src/images/Global_zh-tw/x_y_range/cafe.py
+++ src/images/Global_zh-tw/x_y_range/cafe.py
@@ -4,7 +4,7 @@ x_y_range = {
     'menu': (107, 9, 235, 36),
     '0.0': (1114, 642, 1160, 668),
     'students-arrived': (550, 240, 730, 269),
-    'cafe-reward-status': (490, 132, 800, 174),
+    'cafe-reward-status': (550, 132, 720, 174),
     'invitation-ticket': (421, 78, 451, 111),
     'invitation-ticket-invalid': (480, 309, 780, 355),
     'quick-home': (1215, 5, 1255, 42),
