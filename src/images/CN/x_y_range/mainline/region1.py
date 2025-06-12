67e07b0e77d0ce8e0f72c0dd44bfa7e419de15fd
pur1fy_in_Mac <2274916027@qq.com>
2025-06-13 01:11:11 +0800

    fully remove tutorial support

--- src/images/CN/x_y_range/mainline/region1.py
+++
@@ -1,7 +0,0 @@
-prefix = "mainline"
-path = "mainline/region1"
-x_y_range = {
-    'tutorial': (773, 136, 1008, 182),
-    "training-formation": (87, 3, 310, 35),
-    "tutorial-task-info": (561, 137, 716, 179),
-}
