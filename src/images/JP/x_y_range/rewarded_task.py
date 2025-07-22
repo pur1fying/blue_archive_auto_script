2b6166e825fb8acd546e589c32592cbba39caa2b
pur1fy <2274916027@qq.com>
2025-07-22 22:07:52 +0800

    fix: JP scrimmage and bounty ui change

--- src/images/JP/x_y_range/rewarded_task.py
+++ src/images/JP/x_y_range/rewarded_task.py
@@ -4,7 +4,7 @@ x_y_range = {
     'location-select': (652, 87, 890, 129),
     'task-info': (578, 124, 702, 153),
     'level-list': (862, 98, 999, 123),
-    'purchase-bounty-ticket-notice': (487, 147, 790, 184),
-    'purchase-bounty-ticket-menu': (494, 150, 787, 189),
+    'purchase-bounty-ticket-notice': (487, 163, 790, 198),
+    'purchase-bounty-ticket-menu': (494, 155, 787, 194),
     'help': (583, 112, 691, 148),
 }
