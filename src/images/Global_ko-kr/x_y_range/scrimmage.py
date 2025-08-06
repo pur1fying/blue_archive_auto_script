00d1c57eaafa296f01eef08d0eb9e8ef2e21decf
pur1fy <2274916027@qq.com>
2025-08-06 17:04:21 +0800

    fix : [Global_ko-kr] scrimmage lesson bounty shop ui change

--- src/images/Global_ko-kr/x_y_range/scrimmage.py
+++ src/images/Global_ko-kr/x_y_range/scrimmage.py
@@ -4,6 +4,6 @@ x_y_range = {
     "academy-select": (649, 90, 892, 128),
     'level-list': (862, 98, 999, 123),
     'task-info': (588, 124, 702, 153),
-    'purchase-scrimmage-ticket-menu': (514, 150, 770, 189),
-    'purchase-scrimmage-ticket-notice': (514, 159, 770, 201)
+    'purchase-scrimmage-ticket-menu': (514, 155, 770, 194),
+    'purchase-scrimmage-ticket-notice': (514, 164, 770, 206)
 }
