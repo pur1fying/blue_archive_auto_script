1519e70360a35351d750609192469bb0bd9ab4e7
Nanboom233 <nan_nono_boom@outlook.com>
2025-09-09 23:59:40 +0800

    feat(activity-updater): add SchaleDB as another activity data source.

--- module/activities/activity_updater.py
+++ module/activities/activity_updater.py
@@ -7,20 +7,22 @@ from zoneinfo import ZoneInfo
 import requests
 from lxml import etree
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 "
+                  "Mobile Safari/537.36",
+    "Content-Type": "text/html",
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'game-alias': 'ba'
+}
+
 
 def update_activity_gamekee_api():
     activities = {}
     api_url = "https://www.gamekee.com/v1/activity/query"
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 "
-                      "Mobile Safari/537.36",
-        "Content-Type": "text/html",
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'game-alias': 'ba'
-    }
     Params = {
-        "active_at": int(time.time())
+        # "active_at": int(time.time())
+        "active_at":1755050400
     }
     response = requests.get(
         url=api_url,
@@ -138,33 +140,37 @@ def update_activity_bawiki():
         url=total_assault_url,
         headers=HEADERS,
     )
-    total_assault_html = etree.HTML(total_assault_response.content.decode('utf-8'), parser=etree.HTMLParser(encoding='utf-8'))
-    JP_total_assault  = fetch_activity_table(total_assault_html,'//*[@id="tabber-JP"]/table')
-    Global_total_assault = fetch_activity_table(total_assault_html,'//*[@id="tabber-Global"]/table')
+    total_assault_html = etree.HTML(total_assault_response.content.decode('utf-8'),
+                                    parser=etree.HTMLParser(encoding='utf-8'))
+    JP_total_assault = fetch_activity_table(total_assault_html, '//*[@id="tabber-JP"]/table')
+    Global_total_assault = fetch_activity_table(total_assault_html, '//*[@id="tabber-Global"]/table')
 
     grand_assault_response = requests.get(
         url=grand_assault_url,
         headers=HEADERS,
     )
-    grand_assault_html = etree.HTML(grand_assault_response.content.decode('utf-8'), parser=etree.HTMLParser(encoding='utf-8'))
-    JP_grand_assault  = fetch_activity_table(grand_assault_html,'//*[@id="tabber-JP"]/table')
-    Global_grand_assault = fetch_activity_table(grand_assault_html,'//*[@id="tabber-Global"]/table')
+    grand_assault_html = etree.HTML(grand_assault_response.content.decode('utf-8'),
+                                    parser=etree.HTMLParser(encoding='utf-8'))
+    JP_grand_assault = fetch_activity_table(grand_assault_html, '//*[@id="tabber-JP"]/table')
+    Global_grand_assault = fetch_activity_table(grand_assault_html, '//*[@id="tabber-Global"]/table')
 
     limit_break_assault_response = requests.get(
         url=limit_break_assault_url,
         headers=HEADERS,
     )
-    limit_break_assault_html = etree.HTML(limit_break_assault_response.content.decode('utf-8'), parser=etree.HTMLParser(encoding='utf-8'))
-    JP_limit_break_assault  = fetch_activity_table(limit_break_assault_html,'//*[@id="tabber-JP"]/table')
-    Global_limit_break_assault = fetch_activity_table(limit_break_assault_html,'//*[@id="tabber-Global"]/table')
+    limit_break_assault_html = etree.HTML(limit_break_assault_response.content.decode('utf-8'),
+                                          parser=etree.HTMLParser(encoding='utf-8'))
+    JP_limit_break_assault = fetch_activity_table(limit_break_assault_html, '//*[@id="tabber-JP"]/table')
+    Global_limit_break_assault = fetch_activity_table(limit_break_assault_html, '//*[@id="tabber-Global"]/table')
 
     joint_firing_drill_response = requests.get(
         url=joint_firing_drill_url,
         headers=HEADERS,
     )
-    joint_firing_drill_html = etree.HTML(joint_firing_drill_response.content.decode('utf-8'), parser=etree.HTMLParser(encoding='utf-8'))
-    JP_joint_firing_drill  = fetch_activity_table(joint_firing_drill_html,'//*[@id="tabber-Japanese_version"]/table')
-    Global_joint_firing_drill = fetch_activity_table(joint_firing_drill_html,'//*[@id="tabber-Global_version"]/table')
+    joint_firing_drill_html = etree.HTML(joint_firing_drill_response.content.decode('utf-8'),
+                                         parser=etree.HTMLParser(encoding='utf-8'))
+    JP_joint_firing_drill = fetch_activity_table(joint_firing_drill_html, '//*[@id="tabber-Japanese_version"]/table')
+    Global_joint_firing_drill = fetch_activity_table(joint_firing_drill_html, '//*[@id="tabber-Global_version"]/table')
 
     activities = {
         "JP": {
@@ -189,9 +195,124 @@ def update_activity_bawiki():
     return activities
 
 
+def download_json(url):
+    response = requests.get(
+        url=url,
+        headers=HEADERS,
+    )
+    if response.status_code != 200:
+        print(f"Failed to fetch data from {url}, error code: {response.status_code}")
+        return None
+    return response.json()
+
+
+def check_event_active(server: str, event_info_dict: dict) -> bool:
+    if f"EventOpen{server}" in event_info_dict:
+        event_start = event_info_dict[f"EventOpen{server}"]
+        event_end = event_info_dict[f"EventClose{server}"]
+        return event_start <= time.time() <= event_end
+    else:
+        return False
+
+
+def update_activity_schaledb():
+    events_json = download_json("https://schaledb.com/data/zh/events.min.json")
+    raids_json = download_json("https://schaledb.com/data/zh/raids.min.json")
+    localization_json = download_json("https://schaledb.com/data/zh/localization.min.json")
+    config_json = download_json("https://schaledb.com/data/config.min.json")
+
+    # get event name from localization
+    event_localization_dict = localization_json["EventName"]
+
+    # total assault
+    raid_names_dict = {}
+    for raid in raids_json["Raid"]:
+        raid_id = raid["Id"]
+        raid_name = raid["Name"]
+        raid_names_dict[raid_id] = raid_name
+
+    # limit time assault
+    limit_time_assault_dict = {}
+    for limit_time_assault in raids_json["MultiFloorRaid"]:
+        id = limit_time_assault["Id"]
+        name = limit_time_assault["Name"]
+        limit_time_assault_dict[id] = name
+
+    # joint firing drill
+    joint_firing_drill_dict = {}
+    for joint_firing_drill in raids_json["TimeAttack"]:
+        id = joint_firing_drill["Id"]
+        joint_firing_drill_type = joint_firing_drill["DungeonType"]
+        name = localization_json["TimeAttackStage"][joint_firing_drill_type]
+        joint_firing_drill_dict[id] = name
+
+    activities = {
+    }
+    for server_config in config_json["Regions"]:
+        server = server_config["Name"]
+        current_events = []
+        for event in server_config["CurrentEvents"]:
+            id = str(event["event"])
+            if len(id) > 3:
+                id = id[-3:]
+            start = event["start"]
+            end = event["end"]
+            if start <= time.time() <= end:
+                name = event_localization_dict[id]
+                current_events.append({
+                    "name": name,
+                    "begin_time": str(start),
+                    "end_time": str(end)
+                })
+        current_joint_firing_drill = []
+        current_total_assault = []
+        current_grand_assault = []
+        current_limit_break_assault = []
+        for raid in server_config["CurrentRaid"]:
+            raid_type = raid["type"]
+            id = raid["raid"]
+            start_time = raid["start"]
+            end_time = raid["end"]
+            current_time =  time.time()
+            if current_time < start_time or current_time > end_time:
+                continue
+            if raid_type == "Raid":
+                current_total_assault.append({
+                    "name": raid_names_dict[id],
+                    "begin_time": str(start_time),
+                    "end_time": str(end_time)
+                })
+            elif raid_type == "EliminateRaid":
+                current_grand_assault.append({
+                    "name": raid_names_dict[id],
+                    "begin_time": str(start_time),
+                    "end_time": str(end_time)
+                })
+            elif raid_type == "MultiFloorRaid":
+                current_limit_break_assault.append({
+                    "name": limit_time_assault_dict[id],
+                    "begin_time": str(start_time),
+                    "end_time": str(end_time)
+                })
+            elif raid_type == "TimeAttack":
+                current_joint_firing_drill.append({
+                    "name": joint_firing_drill_dict[id],
+                    "begin_time": str(start_time),
+                    "end_time": str(end_time)
+                })
+        activities[server] = {
+            "event_schedules": current_events,
+            "joint_firing_drill": current_joint_firing_drill,
+            "total_assault": current_total_assault,
+            "grand_assault": current_grand_assault,
+            "limit_break_assault": current_limit_break_assault
+        }
+    return activities
+
+
 def update_activity(baas_thread):
     baas_thread.logger.info("Updating activity...")
     # activities = update_activity_gamekee_api()
-    activities = update_activity_bawiki()
+    activities = update_activity_schaledb()
     print(json.dumps(activities, ensure_ascii=False))
     return True
