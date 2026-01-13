43bf17bdd6997dedb44d27fc2d285859859a6e79
pur1fy <2274916027@qq.com>
2026-01-13 22:19:50 +0800

    fix : Global_en-us ui change

--- src/images/Global_en-us/x_y_range/lesson.py
+++ src/images/Global_en-us/x_y_range/lesson.py
@@ -4,7 +4,7 @@ x_y_range = {
     'select-location': (100, 9, 313, 39),
     'location-select': (100, 9, 200, 39),
     'lesson-information': (528, 100, 745, 135),
-    'all-locations': (538, 100, 742, 135),
+    'all-locations': (538, 80, 742, 135),
     'lesson-report': (538, 120, 747, 158),
     'purchase-lesson-ticket-menu': (461, 154, 819, 190),
     'purchase-lesson-ticket-notice': (461, 170, 819, 205),
