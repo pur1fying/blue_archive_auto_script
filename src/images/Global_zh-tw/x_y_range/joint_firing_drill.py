6b0b364018f6ad132776b74d3df88ba0d1378daf
pur1fy <2274916027@qq.com>
2026-02-02 01:48:31 +0800

    update : joint_firing_drill tempates

--- src/images/Global_zh-tw/x_y_range/joint_firing_drill.py
+++ src/images/Global_zh-tw/x_y_range/joint_firing_drill.py
@@ -3,25 +3,26 @@ path = 'joint_firing_drill'
 x_y_range = {
     "select-drill-menu": (97, 6, 312, 61),
 
-    # "Assault-open": (),
+    "Assault-fighting": (74, 199, 149, 221),
+    "Assault-open": (74, 234, 149, 257),
     # "Assault-lock": (1035, 226, 1181, 321),
     # "Assault-next": (),
 
     # "Defense-open": (),
-    # "Defense-lock": (),
+    "Defense-lock": (649, 255, 792, 289),
     # "Defense-next": (648, 268, 781, 307),
 
     # "Shooting-fighting": (186, 299, 204, 323),
     # "Shooting-open": (96, 162, 190, 181),
     # "Shooting-lock": (),
-    # "Shooting-next": (),
+    "Shooting-next": (1019, 162, 1098, 185),
 
     "Season-Record": (30, 581, 208, 606),
-    "drill-information": (503, 138, 773, 178),
-    "start-drill-notice": (441, 322, 825, 393),
+    "drill-information": (577, 138, 703, 178),
+    "start-drill-notice": (441, 302, 825, 410),
     "drill-finish": (577, 109, 705, 147),
 
-    "sweep-menu": (571, 179, 705, 215),
-    "start-sweep-notice": (587, 141, 700, 179),
-    "sweep-complete": (550, 561, 730, 604),
+    "sweep-menu": (600, 179, 670, 215),
+    "start-sweep-notice": (600, 141, 670, 179),
+    "sweep-complete": (600, 561, 680, 604),
 }
