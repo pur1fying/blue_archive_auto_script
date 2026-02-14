89aab09852ffbc21e8f41f6baea2af186e396bde
pur1fy <2274916027@qq.com>
2026-02-14 17:44:57 +0800

    update : CN activity

--- core/config/default_config.py
+++ core/config/default_config.py
@@ -3541,7 +3541,7 @@ STATIC_DEFAULT_CONFIG = '''
         ]
     },
     "current_game_activity": {
-        "CN": "ShesideOutside",
+        "CN": "FiveDustsFall",
         "Global": "ForWhomTheArtIsForTheFateofDecorativePaintingandAesthetics",
         "JP": "anUnconcealedHeart_2"
     },
