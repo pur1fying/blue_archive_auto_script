a739bcf81629d297aae63d1ab010055ef911d001
pur1fy <2274916027@qq.com>
2026-04-16 17:48:18 +0800

    update : CN activity

--- core/config/default_config.py
+++ core/config/default_config.py
@@ -3541,7 +3541,7 @@ STATIC_DEFAULT_CONFIG = '''
         ]
     },
     "current_game_activity": {
-        "CN": "TripTrapTrain",
+        "CN": "SunlightGirlsNightSong",
         "Global": "ForWhomTheArtIsForTheFateofDecorativePaintingandAesthetics",
         "JP": "CodeBox"
     },
