a340bcb698c3f30b5f1ab79376b1a7e3a2ea0601
pur1fying <2274916027@qq.com>
2026-07-26 16:00:12 +0800

    #523 fix : rewarded_task and scrimmage template change

--- src/images/CN/x_y_range/scrimmage.py
+++ src/images/CN/x_y_range/scrimmage.py
@@ -4,6 +4,6 @@ x_y_range = {
     'academy-select': (653, 88, 889, 127),
     'level-list': (887, 98, 979, 123),
     'task-info': (578, 124, 702, 153),
-    'purchase-scrimmage-ticket-menu': (482, 151, 799, 183),
+    'purchase-scrimmage-ticket-menu': (482, 154, 799, 195),
     'purchase-scrimmage-ticket-notice': (482, 166, 799, 199)
 }
